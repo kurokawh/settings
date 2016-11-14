b326224a39accac16495b4d9bb3c93d313917956
Hiroyuki Kurokawa <hiroyuki.kurokawa@nifty.com>
2016-11-14 12:01:44 +0900

    Move fish_prompt_pwd_dir_length because fish_prompt does not work.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,9 @@
-set fish_prompt_pwd_dir_length 0
-
 function fish_prompt
    set_color --bold
    echo -n "[$HOST:"(prompt_pwd)"]% "
    set_color normal
 end
+set fish_prompt_pwd_dir_length 0
 
 #alias	++	pushd
 #alias	--	popd
